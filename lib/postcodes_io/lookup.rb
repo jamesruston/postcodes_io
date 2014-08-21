731e3e18fb3d9c9895839e7dacc8b393ab3c8f06
James Ruston <jruston90@gmail.com>
2014-08-21 22:31:37 +0100

    added batch postcode lookup

--- lib/postcodes_io/lookup.rb
+++ lib/postcodes_io/lookup.rb
@@ -4,14 +4,59 @@ require 'postcodes_io/postcode'
 
 module Postcodes
   module Lookup
+
+
     def lookup(postcode)
-      postcode.gsub!(/\s+/, '') # remove any whitespace. m1 1ab => m11ab
+      if postcode.class == Array
+        lookup_multiple postcode
+      else
+        lookup_postcode postcode
+      end
+    end
+          
+
+    private
+
+    def lookup_postcode(postcode)
+      postcode = remove_whitespace postcode
       response = Excon.get("https://api.postcodes.io/postcodes/#{postcode}")
+
       unless response.status == 404
         parsed_response = JSON.parse(response.body)
         return Postcodes::Postcode.new(parsed_response['result'])
       end
       return nil
     end
+
+    def lookup_multiple(postcodes)
+      puts "looking up #{postcodes}"
+      payload = {postcodes: postcodes.map {|p| remove_whitespace p}}
+      puts "built payload #{payload.to_json}"
+      response = Excon.post(
+        "https://api.postcodes.io/postcodes",
+         body: payload.to_json,
+         headers: {'Content-Type' => 'application/json'}
+         )
+
+      puts "got response #{response.body}"
+
+      process_response(response) do |r|
+        return r['result'].map do |result|
+          Postcodes::Postcode.new(result['result'])
+        end
+      end
+    end
+
+    def remove_whitespace(string)
+      string.gsub(/\s+/, '') # remove any whitespace. m1 1ab => m11ab
+    end
+
+    def process_response(response, &block)
+      unless response.status == 404
+        yield JSON.parse(response.body)
+      end
+      nil
+    end
+
   end
 end
--- spec/postcode_io_spec.rb
+++ spec/postcode_io_spec.rb
@@ -3,14 +3,16 @@ require 'spec_helper'
 describe Postcodes::IO do
 
   let(:base_url) {'https://api.postcodes.io'}
-  let(:stub_response) { File.read('spec/fixtures/lookup_response.json') }
-
-  before :each do
-    stub_request(:get, "#{base_url}/postcodes/CM129TR")
-    .to_return(status: 200, body: stub_response)
-  end
+  let(:stub_single_response) { File.read('spec/fixtures/lookup_response.json') }
+  let(:stub_multi_response) { File.read('spec/fixtures/lookup_multi_response.json') }
 
   describe '#lookup' do
+
+    before :each do
+      stub_request(:get, "#{base_url}/postcodes/CM129TR")
+        .to_return(status: 200, body: stub_single_response)
+    end
+
     let(:input) { 'CM12 9TR' }
     let(:output) { subject.lookup(input) }
 
@@ -21,6 +23,31 @@ describe Postcodes::IO do
 
     it 'returns the long and lat' do
       output.longitude.should == 0.411227930206834
+    end
+  end
+
+  describe '#lookup with multiple postcodes' do
+
+    before :each do
+      stub_request(:post, "#{base_url}/postcodes")
+        .with(body: postcode_payload, headers: {'Content-Type' => 'application/json'})
+        .to_return(status: 200, body: stub_multi_response)
+    end
+
+    let(:input) { ['OX495NU', 'M320JG', 'NE301DP'] }
+    let(:output) { subject.lookup(input) }
+    let(:postcode_payload) { {postcodes: input}.to_json }
+    
+    it 'makes a request with mutliple postcodes' do
+      output
+      WebMock.should have_requested(:post, "#{base_url}/postcodes")
+    end
+
+    it 'returns a postcode object for each result' do
+      output.count.should == 3
+      output.each do |o|
+        o.class.should == Postcodes::Postcode
+      end
     end
 
   end
